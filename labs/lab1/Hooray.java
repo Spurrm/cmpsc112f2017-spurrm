ffddd4a67ca7c6a27c9b6b35af9c69e97546ae23
Michael Spurr <spurrm@aldenv133.allegheny.edu>
2017-08-31 14:36:44 -0400

    Lab 1 submission

---
+++ labs/lab1/Hooray.java
@@ -0,0 +1,16 @@
+public class Hooray {
+
+    public static void hooray() {
+
+		while(true) { //this will loop as long as Hooray! remains true.
+			System.out.println("Hooray!"); // this line will continously be printed since as long at it is true.
+
+    	} //while
+
+    } //hooray
+
+    public static void main(String[] args) {
+		hooray();
+    } //main
+
+} //Hooray (class)
---
+++ labs/lab1/Weeee.java
@@ -0,0 +1,12 @@
+public class Weeee {
+
+    public static void weeee() {
+		System.out.println("Weeee!"); //this line is printed in the terminal.
+		weeee();
+    } //weeee
+
+    public static void main(String[] args) {
+		weeee();
+    } //main
+
+} //Weeee (class)
